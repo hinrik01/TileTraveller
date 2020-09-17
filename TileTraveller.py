30c3bec06962566b6f5d23a574e1668aae51939f
Jonsteinar <jonss20@ru.is>
2020-09-17 10:44:18 +0000

    nyjast

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,7 +1,12 @@
 # þetta er einfaldur tölvuleikur þar sem notandinn á að fara frá púnt A til púnt B
 block_1 = 1
 block_2 = 1
+def is_valid_dir(diregson):
+    if block_1 == 1 and di:
 
+def directions():
+    #east #south #west #north
+    
 while True:
     you_can_travel = print()
     diragson = input("Direction: ")
@@ -22,9 +27,9 @@ while True:
         if diragson == "w":
             block_1 -= 1
         
-        if block_1 == 1 and block_2 == 1:
+        #if block_1 == 1 and block_2 == 1:
 
         
-        if block_1 == 3 and block_2 == 1:
+        #if block_1 == 3 and block_2 == 1:
 
     print(block_1, block_2)
